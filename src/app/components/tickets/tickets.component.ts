f5a38a93fa51c24820fe7f0a7291ec020a5c84c9
Daniel Asanza Erazo <danielprogramando2000@gmail.com>
2023-07-20 21:21:31 -0500

    Se agrego musica, y se hizo repsonsive

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
   styleUrls: ['./tickets.component.css']
 })
-export class TicketsComponent {
+export class TicketsComponent { 
+  imageUrl: string = 'assets/img/capture.jfif';
+  largeImageUrl: string = 'assets/img/ticketsMovil.jpg';
+  isMobile: boolean = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.isMobile = window.innerWidth < 768; 
+  }
+
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 768;
+  }
 }
